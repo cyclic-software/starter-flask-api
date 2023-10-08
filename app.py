fbfa5f4e02aa680e6453539d415a0257ec174669
posiden <posiden@localhost.localdomain>
2023-10-08 04:13:12 -0400

    added test index.html

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
-import os
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return '''<!doctype html>
+<title>Hello from Flask</title>
+{% if name %}
+  <h1>Hello {{ name }}!</h1>
+{% else %}
+  <h1>Hello, World!</h1>'''
