ff6d9acd6845159d50126ddcb85da8088e235a5b
posiden <posiden@localhost.localdomain>
2023-10-08 04:11:21 -0400

    Revert "testing index.html"
    This reverts commit 1700a66e3a725aa4cadb6949b2b031b882ff246d.

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return '''<!doctype html>
-<title>Hello from Flask</title>
-{% if name %}
-  <h1>Hello {{ name }}!</h1>
-{% else %}
-  <h1>Hello, World!</h1>'''
+    return 'Hello, world!'
